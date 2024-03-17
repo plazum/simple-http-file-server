9b70dcdbf57e0d9edbb71963c225004d9519d612
Plazum Z.R. I <34277374+plazum@users.noreply.github.com>
2024-03-17 14:09:58 +0800

    在文件管理界面添加文件大小和上传时间等信息

--- cgi-bin/file-manager.py
+++ cgi-bin/file-manager.py
@@ -2,6 +2,7 @@ import cgitb
 import codecs
 import os
 import sys
+import time
 from urllib.parse import parse_qs
 
 upload_dir = 'upload/'
@@ -32,14 +33,16 @@ try:
             deleted.append(basename)
         else:
             non_existent.append(basename)
-except:
-    pass
+except EOFError:
+    pass  # 没有POST消息体，即没有要删除的文件
 print('<form method="post">')
-print('<table border="1"><thead><tr><td></td><td><b>文件名</b></td></tr></thead><tbody>')
-file_list = os.listdir('upload')
-for name in file_list:
-    print(f'<tr><td><input type="checkbox" name="filename" value="{name}"></td>'
-          f'<td><a href="../upload/{name}">{name}</a></td></tr>')
+print('<table border="1"><thead><tr><td></td><td><b>文件名</b></td><td><b>大小</b></td><td><b>上传时间</b></td></tr></thead><tbody>')
+with os.scandir(upload_dir) as file_list:
+    for entry in file_list:
+        print(f'<tr><td><input type="checkbox" name="filename" value="{entry.name}"></td>'
+              f'<td><a href="/{upload_dir}{entry.name}">{entry.name}</a></td>'
+              f'<td>{entry.stat().st_size}字节</td>'
+              f'<td>{time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(entry.stat().st_mtime))}</td></tr>')
 print('</tbody></table>')
 print('<br><button>删除选中文件</button>')
 print('</form>')
