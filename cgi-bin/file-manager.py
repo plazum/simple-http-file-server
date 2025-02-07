d7bded86ca4eb7c01b67ea13cc2199cb1b453264
Plazum Z.R. I <34277374+plazum@users.noreply.github.com>
2025-02-07 10:30:41 +0800

    适配Linux系统

--- cgi-bin/file-manager.py
+++ cgi-bin/file-manager.py
@@ -7,13 +7,18 @@ import sys
 import time
 from urllib.parse import parse_qs
 
-upload_dir = 'upload/'
+upload_dir = 'upload'
 cgitb.enable()
 if not os.path.lexists(upload_dir):
-    os.mkdir(upload_dir)
+    # 保存当前umask
+    original_umask = os.umask(0)
+    # 创建目录，设置权限为775，因为在Linux系统上，运行CGI脚本的用户不是当前用户，而是nobody
+    os.mkdir(upload_dir, mode=0o775)
+    # 恢复原始umask
+    os.umask(original_umask)
 # sys.excepthook = lambda exception_type, value, tb: print('<p>', html.escape(str(exception_type)), '<br>', value, '<br>', html.escape(str(tb)), '</p></body></html>')
 sys.stdout = codecs.getwriter('utf-8')(sys.stdout.buffer)
-print('Content-type: text/html; charset=utf-8', end='\r\n')
+print('Content-Type: text/html; charset=utf-8', end='\r\n')
 print(end='\r\n')  # 空行，告诉服务器结束头部
 print('<!DOCTYPE html>')
 print('<html lang="zh">')
@@ -23,27 +28,28 @@ print('<title>文件管理</title>')
 print('</head>')
 print('<body>')
 print('<h2>文件管理</h2>')
-query = ''
+query = {}
 deleted = []
 non_existent = []
 if os.environ.get('REQUEST_METHOD') == 'POST':
-    try:
-        query = parse_qs(input())
+    # 按照内容长度来读取输入，否则在Linux上会读不到EOF，会一直阻塞下去
+    body = sys.stdin.buffer.read(int(os.environ['CONTENT_LENGTH']))
+    query = parse_qs(body.decode('utf-8'))
+    if 'filename' in query:
         for filename in query['filename']:
             basename = os.path.basename(filename)
-            if os.path.exists(upload_dir + basename):
-                os.remove(upload_dir + basename)
+            file_path = os.path.join(upload_dir, basename)
+            if os.path.exists(file_path):
+                os.remove(file_path)
                 deleted.append(basename)
             else:
                 non_existent.append(basename)
-    except EOFError:
-        pass  # 没有POST消息体，即没有要删除的文件
 print('<form method="post">')
 print('<table border="1"><thead><tr><td></td><td><b>文件名</b></td><td><b>大小</b></td><td><b>上传时间</b></td></tr></thead><tbody>')
 with os.scandir(upload_dir) as file_list:
     for entry in file_list:
         print(f'<tr><td><input type="checkbox" name="filename" value="{entry.name}"></td>'
-              f'<td><a href="/{upload_dir}{entry.name}">{entry.name}</a></td>'
+              f'<td><a href="/{upload_dir}/{entry.name}">{entry.name}</a></td>'
               f'<td>{entry.stat().st_size}字节</td>'
               f'<td>{time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(entry.stat().st_mtime))}</td></tr>')
 print('</tbody></table>')
@@ -54,7 +60,7 @@ if len(deleted) != 0:
 if len(non_existent) != 0:
     print(f'<p>以下文件不存在，无需删除：{non_existent}</p>')
 print('<p><a href="../index.html">上传文件</a></p>')
-if query != '':
+if query != {}:
     print(f'<details><p>{query}</p></details>')
 print('</body>')
 print('</html>')
--- cgi-bin/upload.py
+++ cgi-bin/upload.py
@@ -6,13 +6,18 @@ import email.policy
 import os
 import sys
 
-upload_dir = 'upload/'
+upload_dir = 'upload'
 cgitb.enable()
 if not os.path.lexists(upload_dir):
-    os.mkdir(upload_dir)
+    # 保存当前umask
+    original_umask = os.umask(0)
+    # 创建目录，设置权限为775，因为在Linux系统上，运行CGI脚本的用户不是当前用户，而是nobody
+    os.mkdir(upload_dir, mode=0o775)
+    # 恢复原始umask
+    os.umask(original_umask)
 # sys.excepthook = lambda exception_type, value, tb: print('<p>', html.escape(str(exception_type)), '<br>', value, '<br>', html.escape(str(tb)), '</p></body></html>')
 sys.stdout = codecs.getwriter('utf-8')(sys.stdout.buffer)
-print('Content-type: text/html; charset=utf-8', end='\r\n')
+print('Content-Type: text/html; charset=utf-8', end='\r\n')
 print(end='\r\n')  # 空行，告诉服务器结束头部
 print('<!DOCTYPE html>')
 print('<html lang="zh">')
@@ -22,16 +27,18 @@ print('<title>上传文件</title>')
 print('</head>')
 print('<body>')
 print('<h2>上传文件</h2>')
-body = sys.stdin.buffer.read()
+# 按照内容长度来读取输入，否则在Linux上会读不到EOF，会一直阻塞下去
+body = sys.stdin.buffer.read(int(os.environ['CONTENT_LENGTH']))
 header = bytes('Content-Type: ' + os.environ['CONTENT_TYPE'] + '\r\n\r\n', encoding='utf-8')
 multipart_form_data = header + body
 content = email.message_from_bytes(multipart_form_data, policy=email.policy.HTTP)
 for x in content.iter_parts():
     basename = os.path.basename(x.get_filename())
-    if os.path.exists(upload_dir + basename):
+    file_path = os.path.join(upload_dir, basename)
+    if os.path.exists(file_path):
         print(f'<h3>{basename}</h3><p>文件已存在。</p>')
         continue
-    with open(upload_dir + basename, mode='xb') as f:
+    with open(file_path, mode='xb') as f:
         size = f.write(x.get_content())
         print(f'<h3>{basename}</h3><p>{size}字节已写入。</p>')
 print('<p><a href="/">继续上传</a></p>')
